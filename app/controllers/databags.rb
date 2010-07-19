imeyer/chef

CHEF-1312 webui is missing a lot of admin checks

chef-server-webui/app/controllers/databags.rb
@@ -21,7 +21,8 @@ require 'chef' / 'data_bag'
 class Databags < Application
   
   provides :html, :json
-  before :login_required 
+  before :login_required
+  before :is_admin
   
   def new
     @databag = Chef::DataBag.new

chef-server-webui/app/controllers/exceptions.rb
@@ -16,4 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+class AdminAccessRequired < Merb::ControllerExceptions::Unauthorized; end
 
+class Exceptions < Merb::Controller
+  def admin_access_required
+    render :layout => nil
+  end
+end

chef-server-webui/app/controllers/nodes.rb
@@ -25,6 +25,7 @@ class Nodes < Application
   provides :html
   
   before :login_required
+  before :is_admin, :only => [:destroy]
   
   def index
     begin

chef-boneyard/chef-server-webui

app/controllers/databags.rb
@@ -21,7 +21,8 @@ require 'chef' / 'data_bag'
 class Databags < Application
   
   provides :html, :json
-  before :login_required 
+  before :login_required
+  before :require_admin
   
   def new
     @databag = Chef::DataBag.new

app/controllers/exceptions.rb
@@ -16,4 +16,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+class AdminAccessRequired < Merb::ControllerExceptions::Forbidden; end
 
+class Exceptions < Merb::Controller
+  def admin_access_required
+    render :layout => nil
+  end
+end

app/controllers/nodes.rb
@@ -25,6 +25,7 @@ class Nodes < Application
   provides :html
   
   before :login_required
+  before :require_admin, :only => [:destroy]
   
   def index
     begin