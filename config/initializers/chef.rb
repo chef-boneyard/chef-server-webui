920c6cf1e080db2f0a269dd3138a73d35320f35c
Seth Falcon <seth@opscode.com>
2012-09-27 10:10:00 -0700

    Temporarily comment out WebUIUser init at start
    This gets the webui starting. Will need to revisit.

--- config/initializers/chef.rb
+++ config/initializers/chef.rb
@@ -7,11 +7,14 @@ require 'chef/webui_user'
 Chef::Config[:node_name] = Chef::Config[:web_ui_client_name]
 Chef::Config[:client_key] = Chef::Config[:web_ui_key]
 
+# FIXME: commented out the following so that the webui will at least
+# start. Not yet clear where we want to do the initialization
+
 # Create the default admin user "admin" if no admin user exists
-unless Chef::WebUIUser.admin_exist
-  user = Chef::WebUIUser.new
-  user.name = Chef::Config[:web_ui_admin_user_name]
-  user.set_password(Chef::Config[:web_ui_admin_default_password])
-  user.admin = true
-  user.save
-end
+# unless Chef::WebUIUser.admin_exist
+#   user = Chef::WebUIUser.new
+#   user.name = Chef::Config[:web_ui_admin_user_name]
+#   user.set_password(Chef::Config[:web_ui_admin_default_password])
+#   user.admin = true
+#   user.save
+# end
